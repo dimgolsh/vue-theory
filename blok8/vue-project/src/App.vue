6336828facc3f4e898ad6f3dc7d84f3bd2f8bc59
dimon <dimagol64@gmail.com>
2020-04-25 22:21:58 +0300

    input error

--- blok8/vue-project/src/App.vue
+++ blok8/vue-project/src/App.vue
@@ -1,41 +1,42 @@
 <template>
-<div class="container">
-  <form action="">
-  <div class="form-group">
-    <label for="email">Emau</label>
-    <input
-     type="email"
-    id="email"
-    class="form-control"
-    v-model="email"
-    @input="$v.email.$touch()"
-    >
-  </div>
-  </form>
-  <pre>
+  <div class="container">
+    <form action>
+      <div class="form-group">
+        <label for="email">Emau</label>
+        <input
+          type="email"
+          id="email"
+          class="form-control"
+          v-model="email"
+          :class="{'is-invalid' : $v.email.$error}"
+          @input="$v.email.$touch()"
+        />
+        <div class="invalid-feedback" v-if="!$v.email.required">dddddd</div>
+        <div class="invalid-feedback" v-if="!$v.email.email">dddddd</div>
+      </div>
+    </form>
+    <pre>
     {{$v.email}}
   </pre>
-
-</div>
+  </div>
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators'
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
-  data(){
+  data() {
     return {
-      email: ''
-    }
+      email: ""
+    };
   },
   validations: {
     email: {
-     required: required,
-     email
+      required: required,
+      email
     }
   }
-
-}
+};
 </script>
 
 <style>
