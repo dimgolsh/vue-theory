264b6bd1d4ecb35b2a9e17690e120f5eb7541030
dimon <dimagol64@gmail.com>
2020-04-25 23:24:54 +0300

    history back

--- blok8/vue-project/src/pages/Car.vue
+++ blok8/vue-project/src/pages/Car.vue
@@ -1,19 +1,27 @@
 <template>
-  <h1>car id {{id}}</h1>
+  <div class="div">
+    <h1>car id {{id}}</h1>
+    <button class="btn btn-primary" @click="goBack">Back</button>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      id: this.$router.currentRoute.params['id'],
-      idr: this.$route.params['id']
+      id: this.$router.currentRoute.params["id"],
+      idr: this.$route.params["id"]
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.push("/cars");
     }
   },
-  watch:{
-    $route (toR, fromR){
-      this.id = toR.params['id']
+  watch: {
+    $route(toR, fromR) {
+      this.id = toR.params["id"];
     }
   }
-}
+};
 </script>
