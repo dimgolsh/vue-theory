117d571023ab51432e8962c6106b7cb2c2f3b548
dimon <dimagol64@gmail.com>
2020-04-25 23:13:29 +0300

    active link

--- blok8/vue-project/src/App.vue
+++ blok8/vue-project/src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/cars">Cars</router-link>
-      </li>
-       <li class="nav-item">
-        <router-link class="nav-link" to="/form">Form</router-link>
-      </li>
-    </ul>
-  </div>
-</nav>
-    <router-view></router-view>
+      <div class="navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" exact to="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/cars">Cars</router-link>
+          </li>
 
+          <router-link tag="li" class="nav-item" to="/form">
+            <a class="nav-link">Form</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-
-};
+export default {};
 </script>
 
 <style>
