297ec9c670a33f3b4b5dff9917ee07a5f32c980c
dimon <dimagol64@gmail.com>
2020-04-25 22:16:56 +0300

    vue form vuelidate

---
+++ blok8/vue-project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container">
+  <form action="">
+  <div class="form-group">
+    <label for="email">Emau</label>
+    <input
+     type="email"
+    id="email"
+    class="form-control"
+    v-model="email"
+    @input="$v.email.$touch()"
+    >
+  </div>
+  </form>
+  <pre>
+    {{$v.email}}
+  </pre>
+
+</div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  data(){
+    return {
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+     required: required,
+     email
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>
