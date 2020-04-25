7e3f0b7df16c91e4ca81a81fc3d8f375ab012da2
dimon <dimagol64@gmail.com>
2020-04-25 19:54:19 +0300

    props and validation

---
+++ blok3/vue-project/src/Car.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="car">
+    <h3>Name {{carName}}</h3>
+    <h3>Year {{carYear}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["carName", "carYear"],
+  props: {
+    carName: { type: String, default:'rrrrrrr' },
+    carYear: Number
+  },
+
+  computed: {
+    reverseName() {
+      return this.carName
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.car {
+  border: 1px solid black;
+}
+</style>
